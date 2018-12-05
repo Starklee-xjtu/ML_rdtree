bf24cf87d601554d7d8291392fc09713f27ef812
StarkLeeSunny <307908677@qq.com>
2018-12-05 19:03:53 +0800

    3.1.1
    重新加入了0和2load的数据集，
    精确度在0.5以上

--- Fault_class/main.py
+++ Fault_class/main.py
@@ -9,7 +9,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import jaccard_similarity_score
 from sklearn.linear_model import LogisticRegression
 
-pca = PCA(n_components=30)
+pca = PCA(n_components=500)
 rf_per = np.empty([6,1],dtype=float)
 knn_per = np.empty([6,1],dtype=float)
 lg_per = np.empty([6,1],dtype=float)
@@ -27,12 +27,37 @@ wave_train.wave_cut()
 wave_train.add_noise(nos)
 wave_train.trans_norm()
 wave_train.trans_fft()
-X = wave_train.data_cut
-Y = wave_train.label
+X1 = wave_train.data_cut
+Y1 = wave_train.label
 
+wave_train = WaveDate()
+wave_train.load_data(class_name=cfg.CLASS_NAME, position_name=cfg.POSITION_NAME, load_name='2')
+wave_train.wave_cut()
+wave_train.add_noise(nos)
+wave_train.trans_norm()
+wave_train.trans_fft()
+X2 = wave_train.data_cut
+Y2 = wave_train.label
+
+wave_train = WaveDate()
+wave_train.load_data(class_name=cfg.CLASS_NAME, position_name=cfg.POSITION_NAME, load_name='0')
+wave_train.wave_cut()
+wave_train.add_noise(nos)
+wave_train.trans_norm()
+wave_train.trans_fft()
+X0 = wave_train.data_cut
+Y0 = wave_train.label
+
+
+
+X = np.vstack((X1,X2,X0))
+Y = np.hstack((Y1,Y2,Y0))
 X = np.squeeze(X)
+
+
+
 X=pca.fit_transform(X)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+X_train, X_vali, Y_train, Y_vali = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 wave_test = WaveDate()
 wave_test.load_data(class_name=cfg.CLASS_NAME, position_name=cfg.POSITION_NAME, load_name='3')
@@ -40,17 +65,37 @@ wave_test.wave_cut()
 wave_test.add_noise(nos)
 wave_test.trans_norm()
 wave_test.trans_fft()
-X_test1 = wave_test.data_cut
-X_test1 = np.squeeze(X_test1)
-X_test1=pca.transform(X_test1)
-Y_test1 = wave_test.label
+X_test = wave_test.data_cut
+X_test = np.squeeze(X_test)
+X_test=pca.transform(X_test)
+Y_test = wave_test.label
 
 rfc = RandomForestClassifier()
 rfc.fit(X_train, Y_train)
-predict = rfc.predict(X_test1)
-print (rfc.score(X_test1, Y_test1))
+predict = rfc.predict(X_vali)
+print (rfc.score(X_vali, Y_vali))
 predict = rfc.predict(X_test)
 print (rfc.score(X_test, Y_test))
+
+# Fit an independent logistic regression model for each class using the
+# OneVsRestClassifier wrapper.
+base_lr = LogisticRegression(solver='lbfgs',max_iter=10000)
+ovr = OneVsRestClassifier(base_lr)
+ovr.fit(X_train, Y_train)
+
+Y_pred_ovr = ovr.predict(X_vali)
+ovr_jaccard_score = jaccard_similarity_score(Y_vali, Y_pred_ovr)
+print (ovr_jaccard_score)
+
+Y_pred_ovr = ovr.predict(X_test)
+ovr_jaccard_score = jaccard_similarity_score(Y_test, Y_pred_ovr)
+print (ovr_jaccard_score)
+
+
+
+
+
+
 
 #print (pca.explained_variance_ratio_)
 #print (pca.explained_variance_)
