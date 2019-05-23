a8dff868b7908351055fe09a96877f2f67cf7ba2
StarkLeeSunny <307908677@qq.com>
2019-05-23 18:45:43 +0800

    svm 已根据新环境调通
    进一步检查数据结构，应该最多三天可以搞定！

--- Sentiment-Analysis/code/Sentiment_svm.py
+++ Sentiment-Analysis/code/Sentiment_svm.py
@@ -4,21 +4,22 @@ Created on Tue Apr  5 10:05:30 2016
 
 @author: ldy
 """
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from gensim.models.word2vec import Word2Vec
 import numpy as np
 import pandas as pd
 import jieba
 from sklearn.externals import joblib
 from sklearn.svm import SVC
-import sys  
-reload(sys)  
-sys.setdefaultencoding('utf8')
+import sys
+import importlib
+import os
+importlib.reload(sys)
 
 # 加载文件，导入数据,分词
 def loadfile():
-    neg=pd.read_excel('data/neg.xls',header=None,index=None)
-    pos=pd.read_excel('data/pos.xls',header=None,index=None)
+    neg=pd.read_excel('D:\github_rep\ML_rdtree\Sentiment-Analysis/data/neg.xls',header=None,index=None)
+    pos=pd.read_excel('D:\github_rep\ML_rdtree\Sentiment-Analysis/data/pos.xls',header=None,index=None)
 
     cw = lambda x: list(jieba.cut(x))
     pos['words'] = pos[0].apply(cw)
@@ -30,8 +31,8 @@ def loadfile():
 
     x_train, x_test, y_train, y_test = train_test_split(np.concatenate((pos['words'], neg['words'])), y, test_size=0.2)
     
-    np.save('svm_data/y_train.npy',y_train)
-    np.save('svm_data/y_test.npy',y_test)
+    np.save('D:\github_rep\ML_rdtree\Sentiment-Analysis/svm_data/y_train.npy',y_train)
+    np.save('D:\github_rep\ML_rdtree\Sentiment-Analysis/svm_data/y_test.npy',y_test)
     return x_train,x_test
  
 
@@ -54,33 +55,33 @@ def buildWordVector(text, size,imdb_w2v):
 def get_train_vecs(x_train,x_test):
     n_dim = 300
     #Initialize model and build vocab
-    imdb_w2v = Word2Vec(size=n_dim, min_count=10)
+    imdb_w2v = Word2Vec(size=n_dim, min_count=15)
     imdb_w2v.build_vocab(x_train)
     
     #Train the model over train_reviews (this may take several minutes)
-    imdb_w2v.train(x_train)
+    imdb_w2v.train(x_train,epochs=imdb_w2v.epochs, total_examples=imdb_w2v.corpus_count)
     
     train_vecs = np.concatenate([buildWordVector(z, n_dim,imdb_w2v) for z in x_train])
     #train_vecs = scale(train_vecs)
     
-    np.save('svm_data/train_vecs.npy',train_vecs)
-    print train_vecs.shape
+    np.save('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/train_vecs.npy',train_vecs)
+    print (train_vecs.shape)
     #Train word2vec on test tweets
-    imdb_w2v.train(x_test)
-    imdb_w2v.save('svm_data/w2v_model/w2v_model.pkl')
+    imdb_w2v.train(x_test,epochs=imdb_w2v.epochs,total_examples=imdb_w2v.corpus_count)
+    imdb_w2v.save('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data\w2v_model/w2v_model.pkl')
     #Build test tweet vectors then scale
     test_vecs = np.concatenate([buildWordVector(z, n_dim,imdb_w2v) for z in x_test])
     #test_vecs = scale(test_vecs)
-    np.save('svm_data/test_vecs.npy',test_vecs)
-    print test_vecs.shape
+    np.save('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/test_vecs.npy',test_vecs)
+    print (test_vecs.shape)
 
 
 
 def get_data():
-    train_vecs=np.load('svm_data/train_vecs.npy')
-    y_train=np.load('svm_data/y_train.npy')
-    test_vecs=np.load('svm_data/test_vecs.npy')
-    y_test=np.load('svm_data/y_test.npy') 
+    train_vecs=np.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/train_vecs.npy')
+    y_train=np.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/y_train.npy')
+    test_vecs=np.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/test_vecs.npy')
+    y_test=np.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/y_test.npy')
     return train_vecs,y_train,test_vecs,y_test
     
 
@@ -88,14 +89,14 @@ def get_data():
 def svm_train(train_vecs,y_train,test_vecs,y_test):
     clf=SVC(kernel='rbf',verbose=True)
     clf.fit(train_vecs,y_train)
-    joblib.dump(clf, 'svm_data/svm_model/model.pkl')
-    print clf.score(test_vecs,y_test)
+    joblib.dump(clf, 'D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/svm_model/model.pkl')
+    print (clf.score(test_vecs,y_test))
     
     
 ##得到待预测单个句子的词向量    
 def get_predict_vecs(words):
     n_dim = 300
-    imdb_w2v = Word2Vec.load('svm_data/w2v_model/w2v_model.pkl')
+    imdb_w2v = Word2Vec.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/w2v_model/w2v_model.pkl')
     #imdb_w2v.train(words)
     train_vecs = buildWordVector(words, n_dim,imdb_w2v)
     #print train_vecs.shape
@@ -105,23 +106,22 @@ def get_predict_vecs(words):
 def svm_predict(string):
     words=jieba.lcut(string)
     words_vecs=get_predict_vecs(words)
-    clf=joblib.load('svm_data/svm_model/model.pkl')
+    clf=joblib.load('D:\github_rep\ML_rdtree\Sentiment-Analysis\svm_data/svm_model/model.pkl')
      
     result=clf.predict(words_vecs)
     
     if int(result[0])==1:
-        print string,' positive'
+        print (string,' positive')
     else:
-        print string,' negative'
-    
+        print (string,' negative')
 if __name__=='__main__':
     
     
     ##导入文件，处理保存为向量
-#    x_train,x_test=loadfile() #得到句子分词后的结果，并把类别标签保存为y_train。npy,y_test.npy
-#    get_train_vecs(x_train,x_test) #计算词向量并保存为train_vecs.npy,test_vecs.npy
-#    train_vecs,y_train,test_vecs,y_test=get_data()#导入训练数据和测试数据
-#    svm_train(train_vecs,y_train,test_vecs,y_test)#训练svm并保存模型
+    x_train,x_test=loadfile() #得到句子分词后的结果，并把类别标签保存为y_train。npy,y_test.npy
+    get_train_vecs(x_train,x_test) #计算词向量并保存为train_vecs.npy,test_vecs.npy
+    train_vecs,y_train,test_vecs,y_test=get_data()#导入训练数据和测试数据
+    svm_train(train_vecs,y_train,test_vecs,y_test)#训练svm并保存模型
     
 
 ##对输入句子情感进行判断
